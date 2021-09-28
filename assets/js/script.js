7bc6f321379d443c021496b2966bc3abe9fafff4
tcrear <smileybear88@hotmail.com>
2021-09-28 11:33:26 -0500

    Start file share commit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,40 @@
         // If no - we subtract 5 or 10 seconds from the time remaining
         // Once the above is complete, go to the next question
 // After all questions OR after the time runs out - show the user their score
-// High score tracking 
+// High score tracking - local storage
+    // gets the count from local storage in your browser console
+    // "count" at the end can be named anything
+
+
+var timerEl = document.querySelector(".time");
+var questionSectionEl = document.querySelector(".question-section");
+var quizContainer = document.getElementById("quiz");
+var finalScoreEl= document.querySelector(".card");
+var questions =
+
+var questionIndex=0;
+var timerInterval;
+var timeLeft = questions.length*10;
+
+function startGame() {
+    var mainPage= document.getElementById("quiz");
+    mainPage.setAttribute("style", "display: none");
+    timerInterval =setInterval(callback, 1000);
+    displayQuestion();
+}
+function endGame(){
+    clearInterval(timerInterval);
+    timerEl.textContent = "Game over, man!";
+}
+
+function displayQuestion() {
+    questionSectionEl.innerHTML= `  
+    <h2>Question ${questionIndex+1} : ${questions[questionIndex].question} </h2>
+    <ul>
+        <li><button class="answer">${questions[questionIndex].options[0]}</button></li>
+        <li><button class="answer">${questions[questionIndex].options[1]}</button></li>
+        <li><button class="answer">${questions[questionIndex].options[2]}</button></li>
+        <li><button class="answer">${questions[questionIndex].options[3]}</button></li>
+    </ul>
+<div class="message"></div>;
+}
