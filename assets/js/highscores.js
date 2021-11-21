4af620ea41681b158ff2d635703fdd368e3cc8da
tcrear <smileybear88@hotmail.com>
2021-11-20 22:02:25 -0600

    fixed local storage highscore.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,1 +1,19 @@
-var highscore
+var highScoreEl = document.querySelector(".highscores");
+var initials = JSON.parse(localStorage.getItem("intials"));
+
+
+
+
+
+var playAgain = document.querySelector(playagain);
+playAgain.addEventListener("click", function(){
+    location.href="./index.html"
+});
+
+var goBack = document.querySelector(back);
+Back.addEventListener("click", function(){
+    location.href="./index.html"
+});
+
+highscores();
+
